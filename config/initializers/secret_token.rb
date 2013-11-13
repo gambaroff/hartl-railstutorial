bfcdeb6b78a19943f630ea7b7d11a683f4f8018f
Stephania Gambaroff Thomas <stgambar@gambaroff.com>
2013-11-13 12:08:59 -0800

    Augment .gitignore To ignore Rails documentation files, Vim and Emacs swap files, and .DS_Store directories.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'd0fe4c7f6435e3a24a6b6d22d8a2fce8ae45ff48ea16d3cbe1abd886544f8f45f30c50203721e0b4cb397aba0508f837e40b9ce7c919494b1ad0179a429bfd6e'
